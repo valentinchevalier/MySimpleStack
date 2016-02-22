85450c8b9d6785fd87b907476508cd43339e6eda
valentinchevalier <valche.vc@gmail.com>
2016-02-22 10:28:43 +0100

    #1 Resolution de l'exigence de test

---
+++ src/main/java/dcll/vche/MyStack.java
@@ -0,0 +1,40 @@
+package dcll.vche;
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+/**
+ * Created by 21000763 on 17/02/2016.
+ */
+public class MyStack implements SimpleStack {
+
+    /**
+     * Stockage de la stack
+     */
+    private Stack<Item> stack = new Stack<Item>();
+
+    @Override
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+
+    @Override
+    public int getSize() {
+        return stack.size();
+    }
+
+    @Override
+    public void push(Item item) {
+        stack.push(item);
+    }
+
+    @Override
+    public Item peek() throws EmptyStackException {
+        return stack.peek();
+    }
+
+    @Override
+    public Item pop() throws EmptyStackException {
+        return stack.pop();
+    }
+}

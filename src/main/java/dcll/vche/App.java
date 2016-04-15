cc94d6bcaef711e9c6c79ea868a6f581a2be9880
valentinchevalier <valche.vc@gmail.com>
2016-04-15 10:20:45 +0200

    First commit

---
+++ src/main/java/dcll/vche/App.java
@@ -0,0 +1,13 @@
+package dcll.vche;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
